3bcb2dcb765a5474822e46efa7f707d20e43a553
Tim <tokhirov070714@gmail.com>
2024-02-16 20:03:23 +0500

    Created 9 variables

--- Hire LLC/src/App.java
+++ Hire LLC/src/App.java
@@ -3,6 +3,24 @@ public class App
     public static void main(String[] args) throws Exception
     {
         System.out.println("Hello, World!");
-        
     }
+
+    // Variables declaration
+
+    // Company's Information
+    short establishedYear = 2024;
+    byte partnersNum = 4; // Number of Partnering Companies
+
+    // The number of available jobs and potential employees
+    byte jobsNum = 5;
+    byte jobSeekersNum = 10;
+
+    // Employees' data
+
+    // Employee one
+    byte employeeOneId = 23;
+    byte employeeAge = 28;
+    boolean hasDegree = true;
+    byte experienceYears = 3;
+    float employeeGpa = 3.4f;
 }
