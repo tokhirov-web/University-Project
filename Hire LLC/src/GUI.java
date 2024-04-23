8b75e1de08f65aab799ec6cfde79d3f599cdd8f1
Tim <tokhirov070714@gmail.com>
2024-04-23 18:21:27 +0500

    Almost done

---
+++ Hire LLC/src/GUI.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends Frame {
+
+    Frame frame;
+
+    TextField field1;
+    TextField field2;
+
+    Button button;
+
+
+    GUI() {
+        frame = new Frame("Hiring Company");
+
+        field1 = new TextField("Enter the step number that you want to see");
+        field2 = new TextField();
+
+        button = new Button("Click");
+
+        button.setBounds(490, 100, 150, 30);
+
+        field1.setBounds(470, 70, 200, 30);
+        field2.setBounds(800, 70, 350, 150);
+
+
+
+        Perform other = new Perform(this);
+
+        button.addActionListener(other);
+
+
+        frame.add(field1);
+        frame.add(field2);
+
+        frame.add(button);
+
+
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+
+}
+
+
+class Perform implements ActionListener {
+
+    GUI gObj;
+    String userInput;
+
+    Perform(GUI gObj) {
+        this.gObj = gObj;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == gObj.button) {
+            userInput = gObj.field1.getText();
+            System.out.println(userInput);
+        }
+
+        if (userInput == "1") {
+
+        }
+
+    }
+
+}
